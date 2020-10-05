40748274549b3cb1d3da40302b124d97f825c472
leawn <leonrubner2809@gmail.com>
2020-10-05 22:36:18 +0200

    finished that mysql shit today

--- projects/w3/mysqlCreateDatabase.js
+++ projects/w3/mysqlCreateDatabase.js
@@ -10,9 +10,25 @@ const con = mysql.createConnection({
 con.connect( (err) => {
     if (err) throw err;
     console.log('Connected!');
-    const sql = 'ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY';
+    const sql = `UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'`;
+    // const values = [
+    //     ['John', 'Highway 71'],
+    //     ['Peter', 'Lowstreet 4'],
+    //     ['Amy', 'Apple st 652'],
+    //     ['Hannah', 'Mountain 21'],
+    //     ['Michael', 'Valley 345'],
+    //     ['Sandy', 'Ocean blvd 2'],
+    //     ['Betty', 'Green Grass 1'],
+    //     ['Richard', 'Sky st 331'],
+    //     ['Susan', 'One way 98'],
+    //     ['Vicky', 'Yellow Garden 2'],
+    //     ['Ben', 'Park Lane 38'],
+    //     ['William', 'Central st 954'],
+    //     ['Chuck', 'Main Road 989'],
+    //     ['Viola', 'Sideway 1633']
+    //   ];
     con.query(sql, (err, result) => {
         if(err) throw err;
-        console.log('Table altered!');
+        console.log(`${result.affectedRows} record(s) updated`);
     });
 });
